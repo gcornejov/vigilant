2fef38862e16d5eab99020eb50e5a3f6d9803ff6
Gustavo Cornejo <g.cornejovivado@gmail.com>
2025-05-17 21:32:37 -0400

    Add missing docstrings

--- vigilant/run.py
+++ vigilant/run.py
@@ -2,5 +2,8 @@ from vigilant import data_collector, update_spreadsheet
 
 
 def main():
+    """Process for collecting expenses data and load it into a google
+    spreadsheet
+    """
     data_collector.main()
     update_spreadsheet.main()
