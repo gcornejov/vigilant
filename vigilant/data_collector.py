82f81ebcb055c4ffb8e306f2dd01f1a25a505e06
Gustavo Cornejo <g.cornejovivado@gmail.com>
2025-07-16 23:20:39 +0000

    Increment download timeout to 5 seconds

--- vigilant/data_collector.py
+++ vigilant/data_collector.py
@@ -24,7 +24,7 @@ from vigilant.common.values import (
 )
 
 DEFAULT_TIMEOUT: Final[float] = 15.0
-DEFAULT_DOWNLOAD_TIMEOUT: Final[float] = 3.0
+DEFAULT_DOWNLOAD_TIMEOUT: Final[float] = 5.0
 
 storage = (
     GoogleCloudStorage()
